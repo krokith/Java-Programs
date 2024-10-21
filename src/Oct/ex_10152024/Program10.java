97eeedd0c199592e9930133e2a1e24ffe4062122
krokith <krokith20@gmail.com>
2024-10-21 11:31:05 +0530

    javaProgram

---
+++ src/Oct/ex_10152024/Program10.java
@@ -0,0 +1,29 @@
+package Oct.ex_10152024;
+
+import java.util.Scanner;
+
+public class Program10 {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter your Name: ");
+       String name = sc.nextLine();
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int Age = sc.nextInt();
+
+        Scanner sc2 = new Scanner(System.in);
+        System.out.print("Enter your Salary: ");
+        int Salary = sc.nextInt();
+
+
+        System.out.println("My name is : "+name);
+        System.out.println("My Age is : "+Age);
+        System.out.println("My Salary is : "+Salary);
+
+
+
+
+
+    }
+}
